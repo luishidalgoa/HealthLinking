b3734bca17de068f77af94d69b3af899e17ebfa1
Luishidalgoa <luishidalgoa@outlook.es>
2022-12-05 23:31:12 +0100

    0.0

---
+++ src/Modulo/Ejecutable.java
@@ -0,0 +1,13 @@
+package Modulo;
+
+public class Ejecutable {
+    public static void main(String[] args) {
+        Menus menus=new Menus();
+        for(boolean End=false;End==false;){
+            End=menus.Index();
+        }
+    }
+}
+/**
+ * Desarrollar READ capaz de leer cada array de objeto
+ */
